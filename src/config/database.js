8d94b91089ebd561059d6f24f95f71c3cd326944
Patrick Battisti Forsthofer <190200007@estudantes.ips.pt>
2019-12-10 15:16:01 +0000

    URl da database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,14 @@
 require('dotenv/config');
 
-module.exports = {
-	database: process.env.DATABASE_NAME,
-	username: process.env.DATABASE_USERNAME,
-	password: process.env.DATABASE_PASSWORD,
-	host: process.env.DATABASE_HOST,
-	dialect: process.env.DATABASE_DIALECT,
-	define: {
-		timestamps: true,
-	},
-};
+// module.exports = {
+// 	database: process.env.DATABASE_NAME,
+// 	username: process.env.DATABASE_USERNAME,
+// 	password: process.env.DATABASE_PASSWORD,
+// 	host: process.env.DATABASE_HOST,
+// 	dialect: process.env.DATABASE_DIALECT,
+// 	define: {
+// 		timestamps: true,
+// 	},
+// };
+
+module.exports = process.env.DATABASE_URL;
