cc8699e4405ca6a6d8c07eee0dc6256691c3c301
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-02-19 17:33:03 +0000

    fix: Role import

--- src/database/index.js
+++ src/database/index.js
@@ -5,14 +5,24 @@ import sequelizeConfig from '../config/database';
 import User from '../app/models/User';
 import School from '../app/models/School';
 import File from '../app/models/File';
-import Project from "../app/models/Project";
-import SchoolProject from "../app/models/SchoolProject";
-import Country from "../app/models/Country";
-import Activity from "../app/models/Activity";
-import Student from "../app/models/Student";
+import Project from '../app/models/Project';
+import SchoolProject from '../app/models/SchoolProject';
+import Country from '../app/models/Country';
+import Activity from '../app/models/Activity';
+import Student from '../app/models/Student';
 import Role from '../app/models/Role';
 
-const models = [User, School, File, Project, SchoolProject, Country, Activity, Student];
+const models = [
+	User,
+	School,
+	File,
+	Project,
+	SchoolProject,
+	Country,
+	Activity,
+	Student,
+	Role,
+];
 
 class Database {
 	constructor() {
