55a09005ad4a9f2b45144694c2739940c84df581
Patrick Battisti <1729patrick@gmail.com>
2020-02-23 18:02:59 +0000

    fix: Professores find

--- src/app/controllers/ProfessorController.js
+++ src/app/controllers/ProfessorController.js
@@ -1,4 +1,5 @@
 import { Op } from 'sequelize';
+import SchoolProject from '../models/SchoolProject';
 import ProjectUser from '../models/ProjectUser';
 import User from '../models/User';
 import Role from '../models/Role';
@@ -7,13 +8,27 @@ class ProfessorController {
 	async index(req, res) {
 		const { projectId } = req.query;
 
-		const projectUser = await ProjectUser.findAll({ where: { projectId } });
+		const schoolProject = await SchoolProject.findAll({
+			where: {
+				projectId,
+			},
+		});
+
+		const formattedSchoolProject = schoolProject.map(({ id }) => id);
+
+		const projectUser = await ProjectUser.findAll({
+			where: { projectId, userId: { [Op.ne]: null } },
+		});
 
 		const professorsAlreadyProject = projectUser.map(({ userId }) => userId);
 
 		const role = await Role.findOne({ where: { name: 'Professor' } });
 		const users = await User.findAll({
-			where: { roleId: role.id, id: { [Op.notIn]: professorsAlreadyProject } },
+			where: {
+				roleId: role.id,
+				id: { [Op.notIn]: professorsAlreadyProject },
+				schoolId: { [Op.in]: formattedSchoolProject },
+			},
 		});
 
 		return res.json(users);
