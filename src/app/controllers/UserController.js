d5ba10fbcb402b2f215936342a0cb212f9eb8e24
Daniel <iceptalves@gmail.com>
2020-04-17 20:16:05 +0100

    Update: Only send "Teacher" instead of "Professor"

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,6 @@
-import { Op } from 'sequelize';
+import {
+	Op
+} from 'sequelize';
 import CreateUserService from '../services/CreateUserService';
 import User from '../models/User';
 import Role from '../models/Role';
@@ -11,32 +13,52 @@ import ActivationEmail from '../jobs/ActivationEmail';
 class UserController {
 	// Get, Returns all the users in database
 	async index(req, res) {
-		const { role } = req.query;
+		const {
+			role
+		} = req.query;
 
 		let findedRole = {},
-			where = { id: { [Op.ne]: req.userId } };
+			where = {
+				id: {
+					[Op.ne]: req.userId
+				}
+			};
 
 		if (role) {
-			findedRole = await Role.findOne({ where: { name: role } });
+			findedRole = await Role.findOne({
+				where: {
+					name: role
+				}
+			});
 
-			if (findedRole) where = { ...where, roleId: findedRole.id };
+			if (findedRole) where = {
+				...where,
+				roleId: findedRole.id
+			};
 		}
 
 		if (req.role === 'Coordinator' || req.role === 'Professor') {
-			where = { ...where, schoolId: req.schoolId };
+			where = {
+				...where,
+				schoolId: req.schoolId
+			};
 		}
 
 		if (req.role === 'IINTOS-Admin' || req.role === 'IINTOS-Partner') {
-			where = { ...where, roleId: { [Op.or]: [4, 5] } };
+			where = {
+				...where,
+				roleId: {
+					[Op.or]: [4, 5]
+				}
+			};
 		}
 
-		const users = await User.findAll({
+		let users = await User.findAll({
 			where,
 			attributes: {
 				exclude: ['passwordHash'],
 			},
-			include: [
-				{
+			include: [{
 					model: Role,
 					as: 'role',
 					attributes: ['name'],
@@ -55,12 +77,16 @@ class UserController {
 			nest: true,
 		});
 
+		users.forEach((user) => {
+			if (user.role.name === 'Professor') {
+				user.role.name = 'Teacher';
+			}
+		});
 		return res.json(
 			users.map(user => ({
 				...user,
-				certificate: user.certificate.path
-					? `${process.env.APP_URL}/files/${user.certificate.path}`
-					: null,
+				certificate: user.certificate.path ?
+					`${process.env.APP_URL}/files/${user.certificate.path}` : null,
 				role: user.role.name,
 				school: user.school ? user.school.name : null,
 			}))
@@ -69,7 +95,10 @@ class UserController {
 
 	// Post, creates a single user
 	async create(req, res) {
-		const { user, school } = req.body;
+		const {
+			user,
+			school
+		} = req.body;
 
 		const userCreated = await CreateUserService.run({
 			user,
@@ -83,24 +112,41 @@ class UserController {
 	// Delete a user
 	async delete(req, res) {
 		//Find from the route id and deletes the object
-		await User.destroy({ where: { id: req.params.id } });
+		await User.destroy({
+			where: {
+				id: req.params.id
+			}
+		});
 
 		return res.json();
 	}
 
 	// Updates a user
 	async update(req, res) {
-		let { password, oldPassword, ...user } = req.body;
+		let {
+			password,
+			oldPassword,
+			...user
+		} = req.body;
 		let id = oldPassword ? req.userId : req.params.id;
 
-		const ogUser = await User.findOne({ where: { id } });
+		const ogUser = await User.findOne({
+			where: {
+				id
+			}
+		});
 
 		if (oldPassword) {
 			if (!(await ogUser.checkPassword(oldPassword))) {
-				return res.status(401).json({ error: "Password don't match" });
+				return res.status(401).json({
+					error: "Password don't match"
+				});
 			}
 
-			user = { ...user, password };
+			user = {
+				...user,
+				password
+			};
 		}
 
 		const ogActive = ogUser.active;
@@ -121,17 +167,26 @@ class UserController {
 					active: user.active,
 					reasonInactive: user.reasonInactive,
 				},
-				receiver: { email: user.email },
+				receiver: {
+					email: user.email
+				},
 			});
 		}
 
 		const school = await School.findByPk(user.schoolId);
 
 		if (school && school.active !== user.active) {
-			await school.update({ ...school, active: user.active });
+			await school.update({
+				...school,
+				active: user.active
+			});
 		}
 
-		const { passwordHash, password: _, ...restUser } = user;
+		const {
+			passwordHash,
+			password: _,
+			...restUser
+		} = user;
 		//1 because of an null
 		return res.json(restUser);
 	}
