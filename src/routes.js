f5d96531c333af386406f7886df14a7a4d48b825
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-06-13 22:29:13 +0100

    routes

--- src/routes.js
+++ src/routes.js
@@ -33,101 +33,101 @@ const upload = multer(multerConfig);
 
 router.use(publicMiddleware);
 
-router.post('/signup', UserController.create); // create user
-router.post('/sessions', SessionController.create); // Log in
-router.post('/users', authMiddleware, UserController.create); // User created by the admin
+router.post('/api/signup', UserController.create); // create user
+router.post('/api/sessions', SessionController.create); // Log in
+router.post('/api/users', authMiddleware, UserController.create); // User created by the admin
 
 // Middleware to record the logs
 
 router.use(loggerMiddleware);
 
-router.post('/files', upload.single('file'), FileController.create);
-router.get('/schools', SchoolController.index);
+router.post('/api/files', upload.single('file'), FileController.create);
+router.get('/api/schools', SchoolController.index);
 
-router.get('/outputResults', OutputResultController.index);
-router.post('/helpEmail', HelpController.create);
-router.get('/news', NewsController.index);
+router.get('/api/outputResults', OutputResultController.index);
+router.post('/api/helpEmail', HelpController.create);
+router.get('/api/news', NewsController.index);
 
-router.get('/stem', StemController.index);
+router.get('/api/stem', StemController.index);
 
 // Event area
-router.get('/events', EventController.index);
+router.get('/api/events', EventController.index);
 
 // ----- After this point, the user needs autentication -----
 router.use(authMiddleware);
 
-router.post('/events', EventController.create);
-router.put('/events/:id', EventController.update);
-router.delete('/events/:id', EventController.delete);
+router.post('/api/events', EventController.create);
+router.put('/api/events/:id', EventController.update);
+router.delete('/api/events/:id', EventController.delete);
 
 // Output Admin area
-router.post('/outputResults', OutputResultController.create);
-router.delete('/outputResults/:id', OutputResultController.delete);
-router.put('/outputResults/:id', OutputResultController.update);
+router.post('/api/outputResults', OutputResultController.create);
+router.delete('/api/outputResults/:id', OutputResultController.delete);
+router.put('/api/outputResults/:id', OutputResultController.update);
 
-router.get('/log', adminMiddleware, LoggerController.index);
+router.get('/api/log', adminMiddleware, LoggerController.index);
 
 // Stem Admin area
-router.post('/stem', StemController.create);
-router.delete('/stem/:id', StemController.delete);
-router.put('/stem/:id', StemController.update);
+router.post('/api/stem', StemController.create);
+router.delete('/api/stem/:id', StemController.delete);
+router.put('/api/stem/:id', StemController.update);
 
 // News Area
-router.post('/news', NewsController.create);
-router.delete('/news/:id', NewsController.delete);
-router.put('/news/:id', NewsController.update);
-router.post('/resultNews', NewsController.createFromResults);
+router.post('/api/news', NewsController.create);
+router.delete('/api/news/:id', NewsController.delete);
+router.put('/api/news/:id', NewsController.update);
+router.post('/api/resultNews', NewsController.createFromResults);
 
 // User area
-router.get('/users', UserController.index);
-router.put('/users/:id', UserController.update);
-router.delete('/users/:id', adminMiddleware, UserController.delete);
+router.get('/api/users', UserController.index);
+router.put('/api/users/:id', UserController.update);
+router.delete('/api/users/:id', adminMiddleware, UserController.delete);
 
 // School area
-router.post('/schools', adminMiddleware, SchoolController.create);
-router.delete('/schools/:id', adminMiddleware, SchoolController.delete);
-router.put('/schools/:id', SchoolController.update);
+router.post('/api/schools', adminMiddleware, SchoolController.create);
+router.delete('/api/schools/:id', adminMiddleware, SchoolController.delete);
+router.put('/api/schools/:id', SchoolController.update);
 
 // Autentication area
 
 // Project area
-router.get('/projects', ProjectController.index);
-router.get('/projects/:id', ProjectController.findOne);
-router.post('/projects', ProjectController.create);
-router.delete('/projects/:id', ProjectController.delete);
-router.put('/projects/:id', ProjectController.update);
+router.get('/api/projects', ProjectController.index);
+router.get('/api/projects/:id', ProjectController.findOne);
+router.post('/api/projects', ProjectController.create);
+router.delete('/api/projects/:id', ProjectController.delete);
+router.put('/api/projects/:id', ProjectController.update);
 
 // ProjectUser area
-router.get('/projects/:id/users', ProjectUser.index);
-router.post('/projectUser', ProjectUser.create);
-router.get('/projectUser/:projectId/permissions/edit', ProjectUser.canEdit);
-router.delete('/projectUser/:id', ProjectUser.delete);
+router.get('/api/projects/:id/users', ProjectUser.index);
+router.post('/api/projectUser', ProjectUser.create);
+router.get('/api/projectUser/:projectId/permissions/edit', ProjectUser.canEdit);
+router.delete('/api/projectUser/:id', ProjectUser.delete);
 
 // Result area
-router.get('/projects/:id/results', ResultController.index);
-router.post('/results', ResultController.create);
-router.delete('/results/:id', ResultController.delete);
-router.put('/results/:id', ResultController.update);
+router.get('/api/projects/:id/results', ResultController.index);
+router.post('/api/results', ResultController.create);
+router.delete('/api/results/:id', ResultController.delete);
+router.put('/api/results/:id', ResultController.update);
 
 // Activity Area
-router.get('/projects/:id/activities', ActivityController.index);
-router.post('/activities', ActivityController.create);
-router.delete('/activities/:id', ActivityController.delete);
-router.put('/activities/:id', ActivityController.update);
-router.get('/activities', ActivityController.list);
-router.post('/allActivities', ActivityController.createsAll);
+router.get('/api/projects/:id/activities', ActivityController.index);
+router.post('/api/activities', ActivityController.create);
+router.delete('/api/activities/:id', ActivityController.delete);
+router.put('/api/activities/:id', ActivityController.update);
+router.get('/api/activities', ActivityController.list);
+router.post('/api/allActivities', ActivityController.createsAll);
 
 // Roles Area
-router.get('/roles', authMiddleware, RolesController.index);
+router.get('/api/roles', authMiddleware, RolesController.index);
 
-router.get('/professors', ProfessorController.index);
+router.get('/api/professors', ProfessorController.index);
 
 // SchoolProject area
-router.post('/schoolProjects', SchoolProjectController.create);
-router.get('/projects/:id/schools', SchoolProjectController.index);
-router.delete('/schoolProjects/:id', SchoolProjectController.delete);
+router.post('/api/schoolProjects', SchoolProjectController.create);
+router.get('/api/projects/:id/schools', SchoolProjectController.index);
+router.delete('/api/schoolProjects/:id', SchoolProjectController.delete);
 
 // Emails area
-router.post('/sendEmail', EmailController.send);
+router.post('/api/sendEmail', EmailController.send);
 
 export default router;
