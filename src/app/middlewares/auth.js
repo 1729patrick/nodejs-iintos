695edfb4fbb41095d14c4d5d83ec1560209a5169
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-07-22 02:44:26 +0100

    user only permission to get

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -17,6 +17,10 @@ const auth = (req, res, next) => {
 
 	const { userId, role, schoolId } = jwtDecoded;
 
+	if (userId === 91 && req.method !== 'GET') {
+		return res.status(403).json({ error: 'Your user has limited access!' });
+	}
+
 	req.userId = userId;
 	req.schoolId = schoolId;
 	req.role = role;
